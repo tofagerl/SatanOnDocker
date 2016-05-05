cdf76089aa5e6777cf4ef45f3f251a2b7bb8ceb6
Tom Fagerland <tom@fagerland.org>
2016-05-05 20:13:37 +0200

    Renamed image to satan

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export TMK_DIR='/tmk/keyboard/gh60'
 export DOCKER_NAME='satan'
 
 # Build the image from Dockerfile
-docker build -t "${DOCKER_NAME}_image" .
+docker build -t "${DOCKER_NAME}" .
 #
 # Run the script which makes all the firmware
-docker run -it --rm --env TMK_DIR="$TMK_DIR" -v "`pwd`:/work" "${DOCKER_NAME}_image" "$TMK_DIR/make.sh"
+docker run -it --rm --env TMK_DIR="$TMK_DIR" -v "$(pwd):/work" "${DOCKER_NAME}" "$TMK_DIR/make.sh"
